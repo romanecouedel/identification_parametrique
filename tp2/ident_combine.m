c36d6a5cbe8908deaf3721dbbef34bc902f0bde0
Boudemagh Mohamed El Oualid <oualidboudemagh22@gmail.com>
2025-09-19 15:52:16 +0200

    final

--- tp2/ident_combine.m
+++ tp2/ident_combine.m
@@ -1,7 +1,7 @@
 clear all
 load releve_mvts_combines;
-ident_axe1_v_cste
-ident_axe2_v_cste
+% ident_axe1_v_cste
+% ident_axe2_v_cste
 
 %% constantes connues
 kc1=0.0525;
@@ -11,23 +11,23 @@ N2=4.5;
 
 %% Paramètres identifiés à vitesse constante
 %% Pour l'axe 1 : 
-alpha1=p11(1);
-a1=p11(2);
-b1=p11(3);
-c1=p11(4);
+alpha1=0.87;
+a1=0.18;
+b1=0.005;
+c1=-0.03;
 %% Pour l'axe 2 :
-alpha2=p(1);
-a2=p(2);
-b2=p(3);
-c2=p(4);
+alpha2=0.083;
+a2=0.058;
+b2=0.002;
+c2=-0.01;
 
 %% identification à partir des données filtrées
-for(i=1:7992) 
+for(i=1:7992)  
     
-    Z(2*i-1:2*i,1:3)=[qpp1(i) qpp2(i)*cos(q1(i)-q2(i))-(qp2(i)^2)*sin(q2(i)-q1(i)) 0 
-        0 qpp2(i)*cos(q2(i)-q1(i))+(q1(i)^2)*sin(q2(i)-q1(i)) qpp2(i)];
-    u(2*i-1,1)=N1*kc1*i1(i)-alpha1*cos(q1(i))-a1*sign(q1(i))-b1*qp1(i)+c1;
-    u(2*i,1)=N2*kc2*i2(i)-alpha2*cos(q2(i))-a2*sign(q2(i))-b2*qp2(i)+c2;
+    Z(2*i-1:2*i,1:3)=[qpp1(i) qpp2(i)*cos(q1(i)-q2(i))-(qp2(i)^2)*sin(q2(i)-q1(i)) 0;
+        0 qpp1(i)*cos(q2(i)-q1(i))+(qp1(i)^2)*sin(q2(i)-q1(i)) qpp2(i)];
+    u(2*i-1,1)=N1*kc1*i1(i)-alpha1*cos(q1(i))-a1*sign(q1(i))-b1*qp1(i)-c1;
+    u(2*i,1)=N2*kc2*i2(i)-alpha2*cos(q2(i))-a2*sign(q2(i))-b2*qp2(i)-c2;
 end
 
 p=inv(Z'*Z)*Z'*u;
@@ -40,16 +40,16 @@ p1=p(1)
 p2=p(2)
 p3=p(3)
 
-for(i=1:length(t)) 
+for(i=1:7992) 
     %% couple d'inertie
-    ciner(2*i-1,1)=N1*kc1*i1(i); %% AXE 1 
-    ciner(2*i,1)=N2*kc2*i2(i); %%AXE 2
+    ciner(2*i-1,1)= p1*qpp1(i)+p2*qpp2(i)*cos(q2(i)-q1(i)); %% AXE 1 
+    ciner(2*i,1)=p2*cos(q2(i)-q1(i))*qpp1(i)+p3*qpp2(i); %%AXE 2
     %% couple centrifuge
-    ccentri(2*i-1,1)=alpha1*cos(q1(i)); %% AXE 1
-    ccentri(2*i,1)= alpha2*cos(q2(i));
+    ccentri(2*i-1,1)=-p2*sin(q1(i)-q2(i))*(qp2(i))^2; %% AXE 1
+    ccentri(2*i,1)= p3*sin(q2(i)-q1(i))*(qp1(i))^2;
     %% couple de gravité
-    cgravi(2*i-1,1) = a1*sign(q1(i))+b1*qp1(i)+c1; %% AXE 1
-    cgravi(2*i,1) = a2*sign(q2(i))+b2*qp2(i)+c2; %% AXE 2
+    cgravi(2*i-1,1) = alpha1*cos(q1(i)); %% AXE 1
+    cgravi(2*i,1) = alpha2*cos(q2(i)); %% AXE 2
     %% couple de frottements
     cfrott(2*i-1,1)=a1*sign(q1(i))+b1*q1(i)+c1; %% AXE 1
     cfrott(2*i,1)=a2*sign(q2(i))+b2*q2(i)+c2; %% AXE 2
